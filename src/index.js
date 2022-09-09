919cc6a9443c910013f6b223f3104927ce97dbd9
Harold_Mesa <harold.mesa@icloud.com>
2022-09-09 11:19:20 -0500

    Modify template enviroment. Add files to project(.env, exchange_calc.js). Add dotenv-webpack plugin.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './triangle.js';
+import Exchange_calcService from './js/exchange_calc.js';
 
-function handleTriangleForm() {
+
+function exchange_calc() {
+  let promise = Exchange_calcService();
+  promise.then(function(calcDataArray) {
+    printElements(calcDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
+}
+
+function printElements(){
+
+}
+
+function printError() {
+
+}
+
+function handleTriangleForm(event) {
   event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+  exchange_calc();
+  
 }
 
 window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+  document.querySelector("#exchCalc-form").addEventListener("submit", handleTriangleForm);
 });
